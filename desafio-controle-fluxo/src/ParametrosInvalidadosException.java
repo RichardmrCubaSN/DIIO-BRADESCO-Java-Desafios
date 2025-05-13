b64592cfce9840fc4935dba46f6f629994d55986
Richard R. M. Cuba <166261498+RichardmrCubaSN@users.noreply.github.com>
2025-05-13 16:39:50 -0300

    Add files via upload

---
+++ desafio-controle-fluxo/src/ParametrosInvalidadosException.java
@@ -0,0 +1,14 @@
+
+public class ParametrosInvalidadosException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidadosException(String mensagem) {
+        super(mensagem);
+        
+	}
+	
+}
